a937fc452243221f7b4b760960f81341240fdaec
Khôi Nguyễn <originalkhoi151@gmail.com>
2025-01-31 23:52:20 +0700

    Updated 392 sollution, more optimized by iterating through both str

--- IsSubsequence_392_ES.java
+++ IsSubsequence_392_ES.java
@@ -1,19 +1,34 @@
 package com.javalearn;
 
 public class IsSubsequence_392_ES {
+    // public static boolean isSubsequence(String s, String t) {
+    //     if (s.isEmpty()) return true;
+
+    //     int i = 0, j = 0;
+    //     char[] strGiven = s.toCharArray();
+    //     char[] subCheck = t.toCharArray();
+    //     while (j < t.length()) {
+    //         if (i < strGiven.length && strGiven[i] == subCheck[j]) {
+    //             i++;
+    //             j++;
+    //         } else j++;
+    //     }
+    //     return i == s.length();
+    // }
+
     public static boolean isSubsequence(String s, String t) {
         if (s.isEmpty()) return true;
 
-        int i = 0, j = 0;
-        char[] strGiven = s.toCharArray();
-        char[] subCheck = t.toCharArray();
-        while (j < t.length()) {
-            if (i < strGiven.length && strGiven[i] == subCheck[j]) {
-                i++;
-                j++;
-            } else j++;
+        int l = 0, r = 0;
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        while (l < sArr.length && r < tArr.length) {
+            if (sArr[l] == tArr[r]) {
+                l++;
+            }
+            r++;
         }
-        return i == s.length();
+        return l == sArr.length;
     }
 
     public static void main(String[] args) {
